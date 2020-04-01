dd11d6c2e63af666af22700800a7dc9eb42c4685
Ertyoii <iburgy@icloud.com>
2020-04-01 20:02:14 +0800

    parallel data generation

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import sys
 import h5py as h5py
 import torch
+from multiprocessing import Pool, cpu_count
 from torch.utils.data import Dataset
 
 from grasp_robot import *
@@ -24,20 +25,51 @@ class RobotDataset(Dataset):
 def load_data(data_filename):
     f = h5py.File(data_filename, 'r')
     images = np.array(f["input"])
-    images = images.reshape(images.shape[0], 1, images.shape[1], images.shape[2])
+    images = images.reshape((images.shape[0], 1, images.shape[1], images.shape[2]))
 
-    print(images.shape)
     labels = np.array(f["label"])
     f.close()
 
     return images, labels
 
 
-def build_dataset(data_filename, view_matrix, projection_matrix, n):
+def worker(start, end):
+    temp_train = []
+    temp_label = []
+    while start < end:
+        success, seg_img, xya = build_and_grasp(view_matrix, projection_matrix)
+        if success:
+            print(start)
+            temp_train.append(seg_img)
+            temp_label.append(xya)
+            start += 1
+
+    return temp_train, temp_label
+
+
+def build_dataset_parallel():
+    max_threads = cpu_count()
+
+    pool = Pool(processes=max_threads)
+    train, label = zip(*pool.starmap(worker, [(int(volume/max_threads*start), int(volume/max_threads*(start+1))) for start in range(max_threads)]))
+    pool.close()
+
+    train = np.array(train)
+    train = train.reshape((train.shape[0]*train.shape[1], train.shape[2], train.shape[3]))
+    label = np.array(label)
+    label = label.reshape((label.shape[0]*label.shape[1], 3))
+
+    h5 = h5py.File(data_filename, "w")
+
+    h5.create_dataset('input', data=np.array(train))
+    h5.create_dataset('label', data=np.array(label))
+
+
+def build_dataset():
     count = 0
     train = []
     label = []
-    while count < n:
+    while count < volume:
         success, seg_img, xya = build_and_grasp(view_matrix, projection_matrix)
         if success:
             print(count)
@@ -66,8 +98,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     view_matrix, projection_matrix = init(0)
-    volume = 20000
+    volume = cpu_count() * 1000
     data_filename = sys.argv[1]
 
-    build_dataset(data_filename, view_matrix, projection_matrix, volume)
+    build_dataset()
     print("Dataset building completed.")
